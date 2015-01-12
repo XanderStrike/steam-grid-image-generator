f028b8c42f8125f38d24b92cdd9dd4f6346d1475
Alexander Standke <xanderstrike@gmail.com>
2015-01-12 11:10:36 -0800

    mvp

---
+++ gen-image.rb
@@ -0,0 +1,25 @@
+require 'image_suckr'
+require 'RMagick'
+include Magick
+
+print 'Game: '
+game = gets
+
+suckr = ImageSuckr::GoogleSuckr.new
+
+bg_url = suckr.get_image_url({'q' => "#{game} screenshot"})
+
+bg = ImageList.new(bg_url)[0]
+
+bg.resize_to_fill!(460,215)
+
+text_container = Magick::Image.read("label:#{game}") {
+    self.stroke = '#000000'
+    self.fill = '#ffffff'
+    self.size = "#{440}x#{195}"
+    self.background_color = 'transparent'
+}.first
+
+image = bg.composite(text_container, 10, 10, Magick::OverCompositeOp)
+
+image.display
